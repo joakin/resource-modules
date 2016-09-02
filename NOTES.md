7b6fe56f19a69603e3cf0c9a43415de749c7911d
joakin <joaquin@chimeces.com>
2016-09-02 13:39:35 +0200

    Cowboy code initial analysis bullet points
    And add Notes about what we're doing

---
+++ NOTES.md
@@ -0,0 +1,39 @@
+
+extension.json / ResourceModules
+
+    RLmodulename: {
+      dependencies: [ ...RLmodulename ]
+      scripts: [ ...files ]
+      templates: [ ...[id, filename] ]
+      i18n: [ ...keys ]
+    }
+
+From code
+
+    {
+      filename: {
+        requires: [ ...MFmodulename ]
+        mw_requires: [ ...mw.<blah> ]
+        async_requires: [ ...RLmodulename ]
+        defines: [ ...MFmodulename ]
+        templates: [ ...[id, filename] ]
+        i18n: [ ...keys ]
+      }
+    }
+
+## Pre
+* Detect creation of mw.blah.blah = ... and put it in mw_defines
+
+## What
+- Dead files: Check all files are in extension.json
+- Validate templates and i18n in extension.json where the JS file appears
+- Validate order of scripts[] in modulename
+- Unused defines
+* Validate dependencies based on source dependencies
+* Do duplicate entries in extension.json result in duplicate sources in the
+  bundles?
+* Collapsing RL modules into bigger modules
+
+## How
+
+
--- cli.js
+++ cli.js
@@ -13,46 +13,193 @@ const folder = '/Users/jhernandez/dev/wikimedia/mediawiki-vagrant/mediawiki/exte
 
 const testFiles = [
   'resources/mobile.mainMenu/MainMenu.js',
-  // 'resources/mobile.backtotop/backtotop.js',
-  // 'resources/mobile.backtotop/BackToTopOverlay.js',
-  // 'resources/mobile.browser/browser.js',
-  // 'resources/mobile.abusefilter/AbuseFilterOverlay.js',
-  // 'resources/mobile.abusefilter/AbuseFilterPanel.js'
+  'resources/mobile.backtotop/backtotop.js',
+  'resources/mobile.backtotop/BackToTopOverlay.js',
+  'resources/mobile.browser/browser.js',
+  'resources/mobile.abusefilter/AbuseFilterOverlay.js',
+  'resources/mobile.abusefilter/AbuseFilterPanel.js'
 ]
 
-let files = null
-let state = {
-  files: {}
-}
+Promise.all([
+  getFiles(path.join(folder, '/resources'))
+    .then((files) =>
+      files.map((f) => f.replace(folder, '')).filter(isJSFile))
+    .then(getAnalysisFromJSFiles),
+  getResourceModulesInfo(folder)
+])
+  .then(([ana, resourceModules]) => {
 
-getFiles(folder + '/resources')
-  .then((fs) => {
-    files = fs.map((f) => f.replace(folder, ''))
-    return files.filter((f) => f.slice(f.length-3) === '.js')
-  })
-// Promise.resolve(testFiles)
-  .then((jsFiles) =>
-    Promise.all(
-      jsFiles.map((f) =>
-        getSource(folder, f)
-          .then((source) => processFile(state, f, source)))))
-  .then(() => {
-    // Print all state
-    prn(state, true)
+    // Match ana info with extension.json info
+    let errors = {
+      skippedBecauseNotInResourceModules: [],
+      files: {}
+    }
+    Object.keys(ana.files).forEach((file) => {
+      const inModules = getResourceModulesWithFile(file, resourceModules)
+
+      if (inModules.length < 1) {
+        errors.skippedBecauseNotInResourceModules.push(file)
+      } else {
+        // Check that analysis data is included in resourceModules
+        const a = ana.files[file]
+        errors.files[file] = {
+          missingMessages: [],
+          missingTemplates: [],
+          unusedDefines: [],
+          missingDependencies: []
+        }
+
+        // Messages
+        if (a.messages && a.messages.length > 0) {
+          errors.files[file].missingMessages = a.messages.reduce((errs, msg) => {
+            // Modules with missing messages
+            const missing = inModules.filter(([name, module]) => {
+              if (!module.messages) return true
+              if (Array.isArray(module.messages))
+                return module.messages.indexOf(msg) === -1
+              if (module.messages.constructor === Object)
+                return !Object.keys(module.messages)
+                  .some((weirdKey) => module.messages[weirdKey] === msg)
+            })
+            if (missing.length > 0) errs.push([msg, missing])
+            return errs
+          }, [])
+        }
+
+        // Templates
+        if (a.templates && a.templates.length > 0) {
+          // templates: [ { module: RLmodule, fileName: 'Drawer.hogan' } ],
+          errors.files[file].missingTemplates = a.templates.reduce((errs, template) => {
+            // Modules with missing templates
+            const missing = inModules.filter(([name, module]) => {
+              if (!module.templates) return true
+              if (module.templates.constructor === Object)
+                return !Object.keys(module.templates)
+                  .some((tplName) => tplName === template.fileName &&
+                        template.module === module.templates[tplName])
+              throw new Error (`In module ${name}, templates is not an object`)
+            })
+            if (missing.length > 0) errs.push([template, missing])
+            return errs
+          }, [])
+        }
+
+        // Unused defines
+        if (a.defines && a.defines.length) {
+          errors.files[file].unusedDefines = a.defines.filter((mfId) =>
+            // If mfId is not required somewhere with with M.require
+            !Object.keys(ana.files).map((f) => [f, ana.files[f]])
+              .some(([f, fa]) => {
+                if (
+                  (fa.requires && Array.isArray(fa.requires) && fa.requires.indexOf(mfId) > -1) ||
+                  (fa.async_requires && Array.isArray(fa.async_requires) && fa.async_requires.indexOf(mfId) > -1)
+                )
+                  return true
+                else
+                  return false
+              }))
+        }
+
+        // { requires: MFmoduleid
+        //   defines: MFmoduleid
+        //   async_requires: MFmoduleid,
+        //   mw_requires: mw.requires
+      }
+
+    })
+
+    if (errors.skippedBecauseNotInResourceModules.length > 0) {
+      console.log('Warning: Not in extension.json (couldn\'t verify):')
+      console.log(errors.skippedBecauseNotInResourceModules.map((f) => '  ' + f).join('\n'))
+    }
+
+    const filesWithErrors = Object.keys(errors.files)
+      .map((fk) => [fk, errors.files[fk]])
+      .filter(([file, fileErrors]) => {
+        return Object.keys(fileErrors).some((k) => fileErrors[k].length > 0)
+      })
+    filesWithErrors.forEach(([k, f]) => {
+      if (f.missingMessages.length > 0) {
+        const messagesByModule = f.missingMessages.reduce((acc, [msg, modules]) => {
+          modules.forEach(([name]) => {
+            acc[name] = (acc[name] || [])
+            acc[name].push(msg)
+          })
+          return acc
+        }, {})
+
+        console.log(`\nError: Missing messages used directly in file: ${k}:`)
+        console.log(Object.keys(messagesByModule).map((name) =>
+          `  In module ${name}, missing:\n` +
+          messagesByModule[name].map((msg) => '    ' + msg).join('\n')
+        ).join('\n'))
+      }
+
+      if (f.missingTemplates.length > 0) {
+        const templatesByModule = f.missingTemplates.reduce((acc, [template, modules]) => {
+          modules.forEach(([name]) => {
+            acc[name] = (acc[name] || [])
+            acc[name].push(template)
+          })
+          return acc
+        }, {})
+
+        console.log(`\nError: Missing templates used directly in file: ${k}:`)
+        console.log(Object.keys(templatesByModule).map((name) =>
+          `  In module ${name}, missing:\n` +
+          templatesByModule[name].map((template) => '    ' + template.fileName).join('\n')
+        ).join('\n'))
+      }
+
+      if (f.unusedDefines.length > 0) {
+        console.log(`\nError: Unused defines from file: ${k}:`)
+        console.log(f.unusedDefines.map((name) =>
+          `  ${name}`
+        ).join('\n'))
+      }
+    })
+
+    // Print all analysis
+    // prn(ana, true)
+
+    // console.log(Object.keys(ana).length)
+    // console.log(Object.keys(resourceModules))
     
     // Unique sorted mw dependencies
     // console.log(
-    //   Object.keys(state.files)
-    //     .reduce((a, f) => a.concat(state.files[f].mw_requires), [])
+    //   Object.keys(ana.files)
+    //     .reduce((a, f) => a.concat(ana.files[f].mw_requires), [])
     //     .filter((v, k, a) => a.indexOf(v) === k)
     //     .sort()
     //     .join('\n'))
   })
   .catch((e) => console.error(e))
 
-function getFiles (path) {
+function getResourceModulesWithFile (file, resourceModules) {
+  return Object.keys(resourceModules)
+    .filter((rk) => (resourceModules[rk].scripts || []).indexOf(file.replace(/^\//, '')) > -1)
+    .map((rk) => [rk, resourceModules[rk]])
+}
+
+function getResourceModulesInfo (folder) {
+  return getSource(folder, 'extension.json')
+    .then((contents) => JSON.parse(contents).ResourceModules)
+}
+
+function getAnalysisFromJSFiles (jsFiles) {
+  let state = {
+    files: {}
+  }
+  return Promise.all(
+    jsFiles.map((f) =>
+      getSource(folder, f)
+        .then((source) => processFile(state, f, source)))
+  ).then(() => state)
+}
+
+function getFiles (dir) {
   return new Promise((res, rej) =>
-    readDir(path, (err, files) => err ? rej(err) : res(files)))
+    readDir(dir, (err, files) => err ? rej(err) : res(files)))
 }
 
 function getSource (folder, file) {
@@ -76,3 +223,5 @@ function walkAst (state, file, ast) {
   walk.ancestor(ast, matchers(data, file))
   return state
 }
+
+function isJSFile (name) { return name.slice(name.length - 3) === '.js' }
