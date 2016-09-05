06f0156c5e8c57c212eec9a672df08bd78357869
joakin <joaquin@chimeces.com>
2016-09-05 17:33:41 +0200

    Rename parseFile to parse
    Since it gets a string not a file path

--- test/prn-ast.js
+++ test/prn-ast.js
@@ -1,11 +1,11 @@
 const test = require('tape')
 
 const prn = require('../lib/prn-ast')
-const {parseFile} = require('../lib/analyze')
+const {parse} = require('../lib/analyze')
 
 test('returns the ast as text with line and number', (t) => {
   t.equal(
-    prn(parseFile('var a = 1;\n1 + 1')),
+    prn(parse('var a = 1;\n1 + 1')),
     'Line 1 Column 0\n\nvar a = 1;\n1 + 1;\n'
   )
   t.end()
@@ -13,7 +13,7 @@ test('returns the ast as text with line and number', (t) => {
 
 test('when passing justCode=true returns just the source', (t) => {
   t.equal(
-    prn(parseFile('var a = 1;\n1 + 1'), true),
+    prn(parse('var a = 1;\n1 + 1'), true),
     'var a = 1;\n1 + 1;'
   )
   t.end()
