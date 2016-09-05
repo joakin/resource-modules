10bcfc62dec50d416b63a96353cc9021b52951b9
joakin <joaquin@chimeces.com>
2016-09-05 15:54:36 +0200

    test prn and prn-ast

---
+++ test/prn-ast.js
@@ -0,0 +1,20 @@
+const test = require('tape')
+
+const prn = require('../lib/prn-ast')
+const {parseFile} = require('../lib/analyze')
+
+test('returns the ast as text with line and number', (t) => {
+  t.equal(
+    prn(parseFile('var a = 1;\n1 + 1')),
+    'Line 1 Column 0\n\nvar a = 1;\n1 + 1;\n'
+  )
+  t.end()
+})
+
+test('when passing justCode=true returns just the source', (t) => {
+  t.equal(
+    prn(parseFile('var a = 1;\n1 + 1'), true),
+    'var a = 1;\n1 + 1;'
+  )
+  t.end()
+})
---
+++ test/prn.js
@@ -0,0 +1,19 @@
+const test = require('tape')
+
+const prn = require('../lib/prn')
+
+test('returns an nested colored string representation of an object by default', (t) => {
+  t.equal(
+    prn({a: 1, b: 2, c: {a: 1, b: {c: 3}}}),
+    '{ a: \x1b[33m1\x1b[39m, b: \x1b[33m2\x1b[39m, c: { a: \x1b[33m1\x1b[39m, b: { c: \x1b[33m3\x1b[39m } } }'
+  )
+  t.end()
+})
+
+test('returns nothing when passed out=true', (t) => {
+  t.equal(
+    prn({a: 1, b: 2, c: {a: 1, b: {c: 3}}}, true),
+    void 0
+  )
+  t.end()
+})
