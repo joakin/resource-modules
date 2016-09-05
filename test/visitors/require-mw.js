8434100fcd89cd4f00fa1ef2db54e31a02293ee3
joakin <joaquin@chimeces.com>
2016-09-05 19:32:51 +0200

    Test require-mw visitor

---
+++ test/visitors/require-mw.js
@@ -0,0 +1,24 @@
+const test = require('tape')
+
+const {walk} = require('../../lib/analyze')
+const requireMw = require('../../lib/visitors/require-mw')
+
+const files = {
+  'a': {
+    source: `
+    var a = mw.banana + mw.phone.ring('ring')
+    mw.yes.no(mw.config)
+    `
+  }
+}
+
+test('tracks usage of mw.<...> variables', (t) => {
+  t.deepEqual(
+    walk(requireMw, files, 'a').a,
+    {
+      mw_requires: ['mw.banana', 'mw.phone.ring', 'mw.yes.no', 'mw.config'],
+      source: files.a.source
+    }
+  )
+  t.end()
+})
