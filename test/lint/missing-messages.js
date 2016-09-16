04f92a3db863d4ec9bab001dd9dec7553a7cf8cd
joakin <joaquin@chimeces.com>
2016-09-16 13:16:46 +0200

    Type lint/missing-messages.js

--- test/lint/missing-messages.js
+++ test/lint/missing-messages.js
@@ -1,17 +1,20 @@
+// @flow
+
 const test = require('tape')
 
+const {fileAnalysis} = require('../../lib/visitors/types')
 const getMissingMessagesErrors = require('../../lib/lint/missing-messages')
 
 test('won\'t return errors if there are not messages', (t) => {
-  t.equal(getMissingMessagesErrors({}), undefined)
-  t.equal(getMissingMessagesErrors({messages: []}), undefined)
+  t.deepEqual(getMissingMessagesErrors(fileAnalysis({}), []), [])
+  t.deepEqual(getMissingMessagesErrors(fileAnalysis({messages: []}), []), [])
   t.end()
 })
 
 test('it should not complain if messages used in source are in the resource modules', (t) => {
-  t.deepEqual(getMissingMessagesErrors({
+  t.deepEqual(getMissingMessagesErrors(fileAnalysis({
     messages: ['banana', 'phone']
-  }, [
+  }), [
     ['module1', { messages: ['banana', 'phone'] }],
     ['module2', { messages: ['banana', 'phone'] }]
   ]), [])
@@ -24,9 +27,9 @@ test('it should list by message used in source in which resource modules it is n
   const m3 = ['module3', { messages: ['banana'] }]
   const m4 = ['module4', { messages: ['phone', 'ring'] }]
 
-  t.deepEqual(getMissingMessagesErrors({
+  t.deepEqual(getMissingMessagesErrors(fileAnalysis({
     messages: ['banana', 'phone']
-  }, [m1, m2, m3, m4]), [
+  }), [m1, m2, m3, m4]), [
     ['banana',
       [m2, m4]],
     ['phone', [m3]]
@@ -36,9 +39,9 @@ test('it should list by message used in source in which resource modules it is n
 
 test('it should list properly even when messages is a weird object instead of an array', (t) => {
   const m1 = ['module1', { messages: {'0': 'phone', '1': 'ring', 'banana': function () {}} }]
-  t.deepEqual(getMissingMessagesErrors({
+  t.deepEqual(getMissingMessagesErrors(fileAnalysis({
     messages: ['banana', 'phone']
-  }, [m1]), [
+  }), [m1]), [
     ['banana', [m1]]
   ])
   t.end()
