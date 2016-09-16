0be9d8939d2500b9a99fdaf64d88d92aa6310ef1
joakin <joaquin@chimeces.com>
2016-09-16 13:23:20 +0200

    Type lint/missing-templates.js

--- test/lint/missing-templates.js
+++ test/lint/missing-templates.js
@@ -1,17 +1,20 @@
+// @flow
+
 const test = require('tape')
 
+const {fileAnalysis} = require('../../lib/visitors/types')
 const getMissingTemplatesErrors = require('../../lib/lint/missing-templates')
 
 test('won\'t return errors if there are not templates', (t) => {
-  t.equal(getMissingTemplatesErrors({}), undefined)
-  t.equal(getMissingTemplatesErrors({templates: []}), undefined)
+  t.deepEqual(getMissingTemplatesErrors(fileAnalysis({}), []), [])
+  t.deepEqual(getMissingTemplatesErrors(fileAnalysis({templates: []}), []), [])
   t.end()
 })
 
 test('it should not complain if templates used in source are in the resource modules', (t) => {
-  t.deepEqual(getMissingTemplatesErrors({
+  t.deepEqual(getMissingTemplatesErrors(fileAnalysis({
     templates: [{ module: 'module1', fileName: 'Drawer.hogan' }]
-  }, [
+  }), [
     ['module1', { templates: {'Drawer.hogan': './banana/phone.hogan'} }]
   ]), [])
   t.end()
@@ -22,9 +25,9 @@ test('it should list templates used in source that are not in the resource modul
   const s2 = { module: 'module1', fileName: 'Banana.hogan' }
   const s3 = { module: 'module1', fileName: 'Phone.hogan' }
   const m1 = ['module1', { templates: {'Drawer.hogan': './banana/phone.hogan'} }]
-  t.deepEqual(getMissingTemplatesErrors({
+  t.deepEqual(getMissingTemplatesErrors(fileAnalysis({
     templates: [s1, s2, s3]
-  }, [m1]), [
+  }), [m1]), [
     [s2, [m1]],
     [s3, [m1]]
   ])
