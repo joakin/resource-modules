52b6fcddad6e491dea3721df562a4c7fbdd079ac
joakin <joaquin@chimeces.com>
2017-03-06 12:21:01 +0100

    Add error test for M.{require,define} with non-string literals

--- src/test/fixtures/TestMediawiki/extensions/TestExtension/resources/test.js
+++ src/test/fixtures/TestMediawiki/extensions/TestExtension/resources/test.js
@@ -13,10 +13,12 @@ mw.msg( someRandomVariable )
 // Message not declared on ResourceModules
 mw.msg( 'message1' )
 
+mw.mobileFrontend.require(ohSoDynamicRequire)
 mw.mobileFrontend.require('mf-define-in-module-previous-script')
 mw.mobileFrontend.require('mf-define-in-dependency-module')
 mw.mobileFrontend.require('mf-define-not-in-dependencies')
 mw.mobileFrontend.require('mf-define-in-multiple-dependencies')
 mw.mobileFrontend.require('mf-define-nowhere')
 
+mw.mobileFrontend.define(ohSoDynamicRequire, 1)
 mw.mobileFrontend.define('mf-define-unused', 1)
